355e4ba84152b51974c6ba5f2ee4a754d84d16e1
Beilfuss <matheusdicimb@gmail.com>
2025-04-18 09:04:56 +0300

    estrutura try-catch

---
+++ 14-tratamento-excecoes/02-estrutura-try-catch/src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.text.ParseException;
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Input error!");
+		}
+		System.out.println("End of program");
+		
+		sc.close();
+	}
+}
