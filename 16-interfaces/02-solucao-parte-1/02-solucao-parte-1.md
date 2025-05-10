20f4b324e74a223abc6ca2130411802a6c4653f0
Beilfuss <matheusdicimb@gmail.com>
2025-05-10 09:23:11 -0300

    solucao do problema - parte 1

---
+++ 16-interfaces/02-solucao-parte-1/02-solucao-parte-1.md
@@ -0,0 +1,4 @@
+# Solução do problema - PARTE 1
+
+- Solução básica, sem interfaces.
+- Código na pasta `src`.
---
+++ 16-interfaces/02-solucao-parte-1/src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		System.out.println("Enter rental data");
+		System.out.print("Car model: ");
+		String carModel = sc.nextLine();
+		System.out.print("Pickup (dd/MM/yyyy HH:mm): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.print("Return (dd/MM/yyyy HH:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		sc.close();
+	}
+}
